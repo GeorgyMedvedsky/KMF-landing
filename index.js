9d94bede32ce2afc0a7060be9d68d060fed460d3
Georgy Medvedsky <georgymedvedsky@gmail.com>
2024-07-17 16:47:15 +0300

    feat: add heading swiper, refact carousel component

--- index.js
+++ index.js
@@ -47,25 +47,60 @@ function setCarouselFunctionality() {
             CAROUSEL: 'carousel',
             CAROUSEL_LIST: 'carousel__list',
             CAROUSEL_ITEM: 'carousel__item',
-            CAROUSEL_CONTROLS: 'carousel__controls'
+            CAROUSEL_CONTROLS: 'carousel__controls',
+            CAROUSEL_TABS: 'carousel__tabs',
+            CAROUSEL_TAB: 'carousel__tab',
+            CAROUSEL_TAB_ACTIVE: 'carousel__tab_active',
+            CAROUSEL_DOT: 'carousel__dot',
+            CAROUSEL_DOT_ACTIVE: 'carousel__dot_active'
         };
         const carousels = document.querySelectorAll(`.${classes.CAROUSEL}`);
     
         carousels.forEach(carousel => {
+            const tabTemplate = document.querySelector('#tab-template').content;
             const carouselList = carousel.querySelector(`.${classes.CAROUSEL_LIST}`);
             const carouselItems = Array.from(carousel.querySelectorAll(`.${classes.CAROUSEL_ITEM}`));
             const carouselControls = carousel.querySelector(`.${classes.CAROUSEL_CONTROLS}`);
+            const tabList = carouselControls.querySelector(`.${classes.CAROUSEL_TABS}`);
             const prevBtn = carouselControls.querySelector('.prev');
             const nextBtn = carouselControls.querySelector('.next');
             let startX, startY, endX, endY;
-            
             let currentIndex = 0;
             const itemWidth = carouselItems[0].clientWidth;
+            const itemWidthForHeader = (carouselList.offsetWidth * (100 / 100));
+
+            createTabs(carouselItems.length);
+
+            const tabs = Array.from(tabList.querySelectorAll(`.${classes.CAROUSEL_TAB}`));
+            const dots = Array.from(tabList.querySelectorAll(`.${classes.CAROUSEL_DOT}`));
     
-            function updateCarousel(gap) {
-                const offset = -currentIndex * (itemWidth + gap);
-                carouselList.style.transform = `translateX(${offset}px)`;
+            function createTabs(length) {
+                for(let i = 0; i < length; i++) {
+                    const tab = tabTemplate.querySelector(`.${classes.CAROUSEL_TAB}`).cloneNode(true);
+                    tabList.append(tab);
+                }
             }
+
+            function updateCarousel(gap = 16) {
+                const offset = carousel.id !== 'carousel-for-heading'
+                    ? -currentIndex * (itemWidth + gap)
+                    : -currentIndex * (itemWidthForHeader + gap);
+                carouselList.style.transform = `translateX(${offset}px)`;
+                tabs.forEach(tab => tab.classList.remove(classes.CAROUSEL_TAB_ACTIVE));
+                dots.forEach(dot => dot.classList.remove(classes.CAROUSEL_DOT_ACTIVE));
+                tabs[currentIndex].classList.add(classes.CAROUSEL_TAB_ACTIVE);
+                dots[currentIndex].classList.add(classes.CAROUSEL_DOT_ACTIVE);
+            }
+
+            nextBtn.addEventListener('click', () => {
+                if (currentIndex < carouselItems.length - 1) currentIndex++;
+                updateCarousel();
+            });
+    
+            prevBtn.addEventListener('click', () => {
+                if (currentIndex > 0) currentIndex--;
+                updateCarousel();
+            });
     
             carouselList.addEventListener('touchstart', (e) => {
                 startX = e.touches[0].clientX;
@@ -82,17 +117,16 @@ function setCarouselFunctionality() {
             });
     
             carouselList.addEventListener('touchend', () => {
-                // FIXME: исправить перелистывание при простом тапе по элементу
+                // FIXME: исправить баг перелистывания при простом тапе по элементу
                 if (Math.abs(startX - endX) > 50) {
                     if (startX > endX + 50) {
                         if (currentIndex < carouselItems.length - 1) currentIndex++;
                     } else if (startX < endX - 50) {
                         if (currentIndex > 0) currentIndex--;
                     }
-                    updateCarousel(16);
+                    updateCarousel();
                 }
-            });
-    
+            });    
             updateCarousel();
         });
     });
@@ -101,21 +135,3 @@ function setCarouselFunctionality() {
 resetDefaultBehaviorForLinks();
 setMenuFunctionality();
 setCarouselFunctionality();
-
-// nextBtn.addEventListener('click', () => {
-        //     if (currentIndex < carouselItems.length - 1) {
-        //         currentIndex++;
-        //     } else {
-        //         currentIndex = 0;
-        //     }
-        //     updateCarousel();
-        // });
-
-        // prevBtn.addEventListener('click', () => {
-        //     if (currentIndex > 0) {
-        //         currentIndex--;
-        //     } else {
-            //         currentIndex = carouselItems.length - 1;
-        //     }
-        //     updateCarousel();
-        // });
