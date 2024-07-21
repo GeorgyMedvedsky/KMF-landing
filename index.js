4fab3bec0e2db783eb3a353286dab04e69a5faa3
georgy-medvedsky <129301270+GeorgyMedvedsky@users.noreply.github.com>
2024-07-21 18:21:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ function setSliderFunctionality() {
                   sliderList.style.touchAction = 'pan-x';
                 } else {
                   isHorizontalSwipe = false;
-                  sliderList.style.touchAction = 'auto';
+                  sliderList.style.touchAction = 'pan-y';
                 }
                 
             });
